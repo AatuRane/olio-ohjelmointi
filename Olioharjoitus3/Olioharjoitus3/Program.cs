2b142691187be1810a8ac9825b97aa73aa83141a
AatuRane <aatu.rantanen@student.careeria.fi>
2023-04-06 13:42:38 +0300

    Harjoitus3tehty

---
+++ Olioharjoitus3/Olioharjoitus3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Harjoitus_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kiuas kiuas = new Kiuas();
+
+            kiuas.Nimi = "Harvia";
+            kiuas.TulostaData();
+
+            kiuas.KiuasOnOff(true);
+            kiuas.NäytäTila();
+            kiuas.MuutaLämpötilaa(115);
+            kiuas.MuutaKosteutta(25);
+            kiuas.TulostaData();
+
+        }
+    }
+}
---
+++ Olioharjoitusoikea2/Olioharjoitusoikea2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Harjoitus_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opiskelija opiskelija1 = new Opiskelija("Áatu", "DATV22", 0);
+
+            opiskelija1.TulostaData();
+            opiskelija1.MuokkaaOpintopisteitä(5);
+            opiskelija1.TulostaData();
+        }
+    }
+}

c67d51ca415024788deed85a8f90230ade5ed8d7
AatuRane <aatu.rantanen@student.careeria.fi>
2023-04-06 14:01:21 +0300

    Albumitehty

---
+++ Albumiharjoitus4/Albumiharjoitus4/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä4
+{
+    internal class Albumi
+    {
+        public string Artisti;
+        public string Nimi;
+        public string Genre;
+        public int Kappalemäärä;
+        public int Hinta;
+
+        public List<string> kappale = new List<string>();
+
+        public void Kappalelisäys(string kappaleet)
+        {
+            kappale.Add(kappaleet);
+        }
+
+        public void albumitulostus()
+        {
+            Console.WriteLine("Albumi:");
+
+
+            Console.WriteLine("- Artisti: " + Artisti);
+
+            Console.WriteLine("- Albumin nimi: " + Nimi);
+
+            Console.WriteLine("- Genre: " + Genre);
+
+            Console.WriteLine("- Kappaleiden määrä: " + Kappalemäärä);
+
+            Console.WriteLine("- Hinta: " + Hinta + " euroa");
+        }
+
+        public void biisitulostus()
+        {
+            Console.WriteLine("\n--- " + kappale[0] + " 3:34");
+
+            Console.WriteLine("--- " + kappale[1] + " 4:29");
+
+            Console.WriteLine("--- " + kappale[2] + " 6:17");
+
+            Console.WriteLine("--- " + kappale[3] + " 3:33");
+
+            Console.WriteLine("--- " + kappale[4] + " 4:39");
+        }
+
+    }
+}
---
+++ Albumiharjoitus4/Albumiharjoitus4/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tehtävä4;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Albumi albumi = new Albumi();
+        albumi.Artisti = "Eminem";
+        albumi.Nimi = "Is this love";
+        albumi.Genre = "Rap";
+        albumi.Kappalemäärä = 5;
+        albumi.Hinta = 5;
+
+
+        albumi.Kappalelisäys(" From D2 The LBC");
+
+
+        albumi.Kappalelisäys(" Rap God");
+
+
+        albumi.Kappalelisäys(" Cinderella man");
+
+
+        albumi.Kappalelisäys(" The King and I");
+
+
+        albumi.Kappalelisäys(" Last one standing");
+
+        albumi.albumitulostus();
+        albumi.biisitulostus();
+    }
+}

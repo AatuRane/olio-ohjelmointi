b117305d2c2dc055062283a68f790c81c989e62b
AatuRane <aatu.rantanen@student.careeria.fi>
2023-04-17 13:17:56 +0300

    tehtävä 7 tehty

---
+++ Hissi7/Hissi7/Hissi.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hissi7
+{
+    class Hissi
+    {
+        private int kerros;
+
+        private int minKerros;
+        private int maxKerros;
+
+        public int Kerros
+        {
+            set
+            {
+                if (value < minKerros)
+
+
+                {
+                    Console.WriteLine("Tätä kerrosta ei ole. Annettu arvo " + value + " on pienempi kuin " + minKerros);
+                }
+                else if (value > maxKerros)
+
+
+                {
+                    Console.WriteLine("Tätä kerrosta ei ole. Annettu arvo " + value + " on suurempi kuin " + maxKerros);
+                }
+                else
+
+
+                {
+                    kerros = value;
+                }
+            }
+        }
+
+        public Hissi(int _minKerros, int _maxKerros)
+
+
+        {
+            minKerros = _minKerros;
+            maxKerros = _maxKerros;
+        }
+
+        public void TulostaNykyinenKerros()
+
+
+        {
+            Console.WriteLine("Tämän hetkinen kerroksesi " + kerros);
+        }
+
+        public string PalautaMinMax()
+
+
+        {
+            return minKerros.ToString() + "-" + maxKerros.ToString();
+        }
+    }
+}
+
---
+++ Hissi7/Hissi7/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Hissi7
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            Hissi hissi = new Hissi(1, 6);
+
+            int uusiKerros;
+            string i;
+
+            Console.WriteLine("Tästä hissistä voit poistua painamalla 'ulos'.");
+
+            while (true)
+
+
+            {
+                Console.WriteLine();
+                Console.Write("Anna uusi kerros ({0}) < ", hissi.PalautaMinMax());
+
+                i = Console.ReadLine();
+
+                if (i.Equals("ulos"))
+                    break;
+
+                bool tarkistus = int.TryParse(i, out uusiKerros);
+
+                if (tarkistus)
+
+
+                {
+                    hissi.Kerros = uusiKerros;
+                }
+
+                else
+
+
+                {
+                    Console.WriteLine("Tätä kerrosta ei ole. ");
+                }
+
+                hissi.TulostaNykyinenKerros();
+
+
+            }
+
+            Console.WriteLine("Poistuit hissistä!");
+        }
+    }
+}
